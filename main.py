5ba63237586cad3c37fd14c41e5fdfbd461d35c6
Chizzle_001 <chirag1346@gmail.com>
2023-07-30 05:28:06 +0530

    Model Trainer Added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from WineQualityPred import logger
 from WineQualityPred.pipeline.stage01_Data_ingestion import DataIngestionTrainingPipeline
 from WineQualityPred.pipeline.stage02_Data_Validation import DataValidationTrainingPipeline
+from WineQualityPred.pipeline.stage03_Data_Transformation import DataTransformationTrainingPipeline
+from WineQualityPred.pipeline.stage04_Model_Trainer import ModelTrainerTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -23,3 +26,36 @@ except Exception as e:
         logger.exception(e)
         raise e
      
+     
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+     
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   Model_trainer = ModelTrainerTrainingPipeline()
+   Model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+"""     
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+"""
